7163d25ae3da94a1013869994dfceee9f8d7fc2a
Touchie771 <mihai.constantin.bara@gmail.com>
2025-11-14 09:09:39 +0200

    initial commit

---
+++ src/main/java/me/touchie771/voting/Voting.java
@@ -0,0 +1,17 @@
+package me.touchie771.voting;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Voting extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
